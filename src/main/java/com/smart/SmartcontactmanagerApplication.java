d72091f214b4429a0af6a06e66b2df96538016a7
ShivajiPawar <shivajipawar1928@gmail.com>
2025-03-22 10:50:14 +0530

    initial commit

---
+++ src/main/java/com/smart/SmartcontactmanagerApplication.java
@@ -0,0 +1,22 @@
+package com.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SmartcontactmanagerApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(SmartcontactmanagerApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcontactmanagerApplication.class, args);
+		
+	}
+
+}
